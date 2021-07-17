dcb8779d7d2ad1bffe15bc7c9e6d4264f1217e51
Ng Wei Han <weihan774237@gmail.com>
2021-07-17 18:06:45 +0800

    Initial commit

---
+++ app/src/main/java/com/example/matcha_pasta/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.matcha_pasta;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
