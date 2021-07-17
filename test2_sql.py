a6bff67beaae86f34c768784e3ebbc12517f778e
Kar Kei Ong <kong0016@student.monash.edu>
2021-07-18 06:11:43 +0800

    Added test files for database

---
+++ test2_sql.py
@@ -0,0 +1,5 @@
+from QuickHack_2021.sqlite import *
+
+id  = get_user_id('la', 'le')
+print(id)
+get_image(id)
---
+++ test_sql.py
@@ -0,0 +1,6 @@
+import sqlite3
+
+from QuickHack_2021.sqlite import *
+
+insert_user('la', 'le')
+insert_family(get_user_id('la', 'le'), '8.png', 'wei han', 'ng')
