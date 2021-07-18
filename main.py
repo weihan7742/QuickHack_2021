623ced9babfbd665d96d59b408cde5c065a06ce4
cchwee <cc.cassandrachwee@gmail.com>
2021-07-18 14:54:47 +0800

    Added Screen Manager & initial Setup of Profile Screen

--- main.py
+++ main.py
@@ -3,9 +3,25 @@
 # Creating the App
 
 from kivymd.app import MDApp
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
+
+
+# declare screens
+class WelcomeScreen(Screen):
+    pass
+
+class ProfileScreen(Screen):
+    pass
+
+class ScreenManager(ScreenManager):
+    pass
+
 
 class iKnowU(MDApp):
     def build(self):
-        return
+        buildkv = Builder.load_file("main.kv")
+        return buildkv
 
-iKnowU().run()
+if __name__ == "__main__":
+    iKnowU().run()
